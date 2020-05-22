12b12721c6d60706875ea39d04299836e6da0f17
Pradius <josecamilo@outlook.com>
2020-05-22 10:46:11 -0400

    Raindrops Challenge

--- java/raindrops/src/main/java/RaindropConverter.java
+++ java/raindrops/src/main/java/RaindropConverter.java
@@ -1,7 +1,21 @@
 class RaindropConverter {
 
     String convert(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if ((number % 3) != 0 && (number % 5) != 0 && (number % 7) != 0) {
+            return Integer.toString(number);
+        }
+        var output = new StringBuilder();
+
+        if ((number % 3) == 0) {
+            output.append("Pling");
+        }
+        if ((number % 5) == 0) {
+            output.append("Plang");
+        }
+        if ((number % 7) == 0) {
+            output.append("Plong");
+        }
+        return output.toString();
     }
 
 }
