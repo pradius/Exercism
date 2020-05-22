97d787dae33ee1678cd4d04e08932a4db597ea5a
Pradius <josecamilo@outlook.com>
2020-05-22 13:56:03 -0400

    Created Pig Latin challenge

--- java/isogram/src/main/java/IsogramChecker.java
+++ java/isogram/src/main/java/IsogramChecker.java
@@ -1,7 +1,24 @@
+import java.util.HashSet;
+import java.util.Set;
+
+
 class IsogramChecker {
 
     boolean isIsogram(String phrase) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        Set bucket = new HashSet();
+
+        for (var letter : phrase.toLowerCase().toCharArray()){
+            if (letter == ' ' || letter == '-'){
+                continue;
+            }
+            else if (bucket.contains(letter)){
+                return false;
+            }
+            else{
+                bucket.add(letter);
+            }
+        }
+        return true;
     }
 
 }
