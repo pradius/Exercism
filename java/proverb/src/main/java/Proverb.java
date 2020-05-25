d3328f3725845b244afa8370204d261674285bc6
Pradius <josecamilo@outlook.com>
2020-05-25 14:00:47 -0400

    Created Proverb problem

---
+++ java/proverb/src/main/java/Proverb.java
@@ -0,0 +1,12 @@
+class Proverb {
+
+    String[] words;
+    Proverb(String[] words) {
+       this.words = words;
+    }
+
+    String recite() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+}
