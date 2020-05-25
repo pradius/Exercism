054ee3b8e79a4dfe0fc04e2446b1a46d83a29fad
Pradius <josecamilo@outlook.com>
2020-05-25 14:31:18 -0400

    Completed Proverb Problem

--- java/proverb/src/main/java/Proverb.java
+++ java/proverb/src/main/java/Proverb.java
@@ -6,7 +6,15 @@ class Proverb {
     }
 
     String recite() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if(words.length == 0)return "";
+        StringBuilder proverb = new StringBuilder();
+        if(words.length > 1){
+            for (int i = 0; i < words.length - 1; i++){
+                proverb.append(String.format("For want of a %s the %s was lost.\n", words[i], words[i+1]));
+            }
+        }
+        proverb.append(String.format("And all for the want of a %s.", words[0]));
+        return proverb.toString();
     }
 
 }
